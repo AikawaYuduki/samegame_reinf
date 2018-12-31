9c2db47449eaccbd22b1a3cea79d56a2d8342b25
aikawa_yuduki <tama.pen.sula2240@gmail.com>
2018-12-31 23:54:57 +0900

    なんか500回くらいで必ずエラー吐くから、メモリとかの問題なのかな？（なんかそんな記述が合ったような気がする）

--- samegame_reinf/samegame_reinf.py
+++ samegame_reinf/samegame_reinf.py
@@ -1,1 +1,82 @@
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import chainerrl
+import numpy as np
+import smgm
+from random import randint
 
+#盤面をRGBにします！！！！
+
+class QFunction(chainer.Chain):    
+    def __init__(self, n_actions):
+        super().__init__(
+            L0=L.Convolution2D(3 , 16, ksize=8, stride=1),
+            L1=L.Convolution2D(16, 32, ksize=4, stride=1),
+            L2=L.Convolution2D(32, 32, ksize=3, stride=1),
+            L3=L.Linear(2688, 512),
+            L4=L.Linear(512, n_actions))
+ 
+    def __call__(self, x, test=False):
+        h = F.relu(self.L0(x))
+        h = F.relu(self.L1(h))
+        h = F.relu(self.L2(h))
+        h = F.relu(self.L3(h))
+        return chainerrl.action_value.DiscreteActionValue(self.L4(h))
+    
+def random_action():
+    return np.random.randint(0,obs_size,dtype="int")
+
+same = smgm.Game()
+
+# 環境と行動の次元数
+obs_size = same.raw * same.line
+n_actions = same.raw * same.line
+# Q-functionとオプティマイザーのセットアップ
+q_func = QFunction(n_actions)
+optimizer = chainer.optimizers.Adam(eps=1e-4)
+optimizer.setup(q_func)
+# 報酬の割引率
+gamma = 0.95
+# Epsilon-greedyを使ってたまに冒険。50000ステップでend_epsilonとなる
+explorer = chainerrl.explorers.LinearDecayEpsilonGreedy(
+    start_epsilon=1.0, end_epsilon=0.3, decay_steps=50000, random_action_func=random_action)
+# Experience ReplayというDQNで用いる学習手法で使うバッファ
+replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10 ** 6)
+# Agentの生成（replay_buffer等を共有する2つ）
+agent = chainerrl.agents.DoubleDQN(
+    q_func, optimizer, replay_buffer, gamma, explorer,
+    replay_start_size=500, update_interval=1,
+    target_update_interval=100)
+
+#学習ゲーム回数
+n_episodes = 20000
+n_turn = 300
+
+for i in range(1, n_episodes + 1):
+    same.make_board()
+    reward = 0
+    turns = 0
+    while turns < 300:
+        #配置マス取得
+        b = same.torgb()
+        action = agent.act_and_train(b, reward)
+        #配置を実行
+        same.click(action)
+        #配置の結果、終了時には報酬とカウンタに値をセットして学習
+        if same.is_clear() == True:
+            reward = same.score()
+            #エピソードを終了して学習
+            agent.stop_episode_and_train(same.board.copy(), reward, True)
+            break
+        else:
+            turns += 1
+
+    #コンソールに進捗表示
+    if i % 100 == 0:
+        print("episode:", i, " / reward:", reward ," / statistics:", agent.get_statistics(), " / epsilon:", agent.explorer.epsilon)
+    if i % 1000 == 0:
+        # 10000エピソードごとにモデルを保存
+        agent_p1.save("result_" + str(i))
+
+print("Training finished.")
--- samegame_reinf/smgm.py
+++ samegame_reinf/smgm.py
@@ -1,1 +1,178 @@
+import random
+import numpy as np
 
+#盤の縦横サイズ
+RAW = 15
+LINE = 40
+
+#ゲームのクラス
+class Game:
+    def __init__(self,raw=RAW,line=LINE):
+        self.raw = raw
+        self.line = line
+        self.n_mass = self.raw*self.line
+        self.make_board()
+
+    #盤面を作る
+    def make_board(self):
+        self.board = [0 for i in range(self.n_mass)]
+
+        #色は1,2,3,4 0は空白
+        for i in range(self.n_mass):
+            self.board[i] = (i%4) + 1
+        random.shuffle(self.board)
+
+        return self.board
+
+    #上下左右に盤面があるかの判定
+    def exist_board(self,pos):
+        self.ex_bor = {"right":True,"up":True,"left":True,"down":True}
+        #上
+        if pos < self.raw:
+            self.ex_bor["up"] = False
+        #左
+        if pos%self.line == 0:
+            self.ex_bor["left"] = False
+        #右
+        if (pos+1)%self.line == 0:
+            self.ex_bor["right"] = False
+        #下
+        if (pos // self.line) == (self.raw - 1):
+            self.ex_bor["down"] = False
+
+        return self.ex_bor
+
+    #隣が同じ色か
+    def samecolor_a(self,pos):
+        #右
+        if self.exist_board(pos)["right"]:
+            if not self.checked[pos+1]:
+                if self.board[pos] == self.board[pos+1]:
+                    self.checked[pos+1] = True
+                    self.sames.append(pos+1)
+                    self.samecolor_a(pos+1)
+
+        #上
+        if self.exist_board(pos)["up"]:
+            if not self.checked[pos-self.line]:
+                if self.board[pos] == self.board[pos-self.line]:
+                    self.checked[pos-self.line] = True
+                    self.sames.append(pos-self.line)
+                    self.samecolor_a(pos-self.line)
+
+        #左
+        if self.exist_board(pos)["left"]:
+            if not self.checked[pos-1]:
+                if self.board[pos] == self.board[pos-1]:
+                    self.checked[pos-1] = True
+                    self.sames.append(pos-1)
+                    self.samecolor_a(pos-1)
+
+        #下
+        if self.exist_board(pos)["down"]:
+            if not self.checked[pos+self.line]:
+                if self.board[pos] == self.board[pos+self.line]:
+                    self.checked[pos+self.line] = True
+                    self.sames.append(pos+self.line)
+                    self.samecolor_a(pos+self.line)
+
+
+    def samecolor(self,pos):
+        #チェック済みかのリストを初期化
+        self.checked = [False for i in range(self.n_mass)]
+        #同じ色リストを初期化
+        self.sames = [pos]
+
+        self.samecolor_a(pos)
+
+        return self.sames
+
+    #同じやつを消す
+    def delete(self,pos):
+        for i in self.samecolor(pos):
+            self.board[i] = 0
+
+    #横を詰める
+    def yoko_tsume(self):
+        tsumetakana = False
+        #空じゃない列
+        empline = range(self.line)
+        for i in range(self.line):
+            #縦のリストを作ります
+            self.tate_pos = [(j*self.line)+i for j in range(self.raw)]
+            self.tate = [self.board[j] for j in self.tate_pos]
+            #詰める
+            #すべてが0かという判定
+            if all([x==0 for x in self.tate]):
+                #空の列は除きましょうね
+                empline.remove(i)
+                tsumetakana = True
+                
+        #空じゃない列を左から詰めて再描写
+        if tsumetakana:
+            for i in range(self.line):
+                if i+1 <= len(empline):
+                    for j in range(self.raw):
+                        self.board[(self.line*j)+i] = self.board[(self.line*j)+empline[i]]
+                else:
+                    for j in range(self.raw):
+                        self.board[(self.line*j)+1] = 0
+
+                 
+    def tate_tsume(self):
+        for i in range(self.line):
+            tsumetakana = False
+            #縦のリストを作ります
+            self.tate_pos = [(j*self.line)+i for j in range(self.raw)]
+            self.tate = [self.board[j] for j in self.tate_pos]
+            #空のますを抜いてしたからつめて再描写。
+            self.tate = [j for j in self.tate if not j == 0]
+            for j in range(self.raw):
+                if j+1 <= len(self.tate):
+                    self.board[((self.raw-j-1)*self.line)+1] = self.tate[-(j+1)]
+                else:
+                    self.board[((self.raw-j-1)*self.line)+1] = 0
+
+    #クリアしたかな？
+    def is_clear(self):
+        self.done = False
+        for i in range(self.n_mass):
+            if self.exist_board(i)["right"]:
+                self.done = True
+            if self.exist_board(i)["up"]:
+                self.done = True
+            if self.exist_board(i)["left"]:
+                self.done = True
+            if self.exist_board(i)["down"]:
+                self.done = True
+            
+        return self.done
+
+    def click(self,pos):
+        self.samecolor(pos)
+        self.delete(pos)
+        self.yoko_tsume()
+        self.tate_tsume()
+
+        return self.board
+
+    def score(self):
+        self.scores = 0
+        for i in self.board:
+            if i == 0:
+                self.scores += 1
+
+        return self.scores
+
+    def torgb(self):
+        colors = {0:[0,0,0],1:[1,0,0],2:[0,1,0],3:[0,0,1],4:[1,1,1]}
+        rgbboard = [0 for i in range(self.n_mass*3)]
+        for i in range(self.n_mass):
+            rgbboard[i] = colors[self.board[i]][0]
+            rgbboard[i+(self.n_mass)] = colors[self.board[i]][1]
+            rgbboard[i+(self.n_mass*2)] = colors[self.board[i]][2]
+
+        rgbboard = np.array(rgbboard,dtype="float32")
+        rgbboard = rgbboard.reshape([3,self.raw,self.line])
+
+        return rgbboard
